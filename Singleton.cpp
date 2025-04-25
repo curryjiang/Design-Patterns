4dbcea10c93f9b7a95da8296a4620e915752219a
jiangtao <2606361057@qq.com>
2025-04-25 23:10:20 +0800

    first commit

---
+++ Singleton.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <mutex>
+using namespace std;
+
+// 线程安全的懒汉模式
+class Singleton
+{
+private:
+    Singleton() = default;
+    ~Singleton() = default;
+
+public:
+    static Singleton &getInstance()
+    {
+        static Singleton m_instance;
+        return m_instance;
+    }
+
+    void dosomething()
+    {
+        cout << "do something" << endl;
+    }
+
+    Singleton(const Singleton &other) = delete;
+    Singleton &operator=(const Singleton &other) = delete;
+};
+
+int main()
+{
+    Singleton& s = Singleton::getInstance();
+    s.dosomething();
+    return 0;
+}
